e4de0d3db5d8e665da2be4bdd05e3b884ae37114
Dimitris Vidos <dvidos@gmail.com>
2022-04-10 19:24:52 -0400

    Protected mode running, message top of screen

--- bootable/boot_sector.asm
+++ bootable/boot_sector.asm
@@ -1,7 +1,8 @@
 ; boot sector is 512 bytes long,
 ; last two bytes need to have the values 0xAA55 for it to detect a bootable sector
 ; it is loaded by BIOS on memory address 0x7c00 to 0x7dff
-[org 0x7C00]   
+[org 0x7C00]
+[bits 16]
 
 ; square brackets convert a value to a memory pointer
 ; move a number to al
@@ -36,57 +37,51 @@
 ; we pass arguments and get returned values in registers
 ; we use pusha and popa to preserve registers and avoid messing the caller's values
     
-    mov bx, hello_message  ; essentially move the address of the label
+    mov bx, real_mode_message  ; essentially move the address of the label
     call print_string
     call print_crlf
 
-    ; dl should have the drive number
-    mov dh, 0
-    mov dl, dl
-    call print_hex 
-    call print_crlf
+;    ; dl should have the drive number
+;    mov dh, 0
+;    mov dl, dl
+;    call print_hex 
+;    call print_crlf
+;
+;    ; jmp $
+;
+;    ; let's load some more sectors, onto 0x9000 (remember stack at 0x8000)
+;    mov dl, dl      ; drive to load, already set by BIOS
+;    mov dh, 2       ; how many sectors to load
+;    mov bp, 0x0000  ; make sure es has zero
+;    mov es, bp
+;    mov bx, 0x9000  ; es:bx point to where to load the sectors
+;    call load_sectors
 
-    ; jmp $
-
-    ; let's load some more sectors, onto 0x9000 (remember stack at 0x8000)
-    mov dl, dl      ; drive to load, already set by BIOS
-    mov dh, 2       ; how many sectors to load
-    mov bp, 0x0000  ; make sure es has zero
-    mov es, bp
-    mov bx, 0x9000  ; es:bx point to where to load the sectors
-    call load_sectors
-
-    ; check byte in each segment
-    mov dx, 0
-    mov dl, [0x9000]
-    call print_hex
-    call print_crlf
-    mov dl, [0x9000 + 511]
-    call print_hex
-    call print_crlf
-    mov dl, [0x9000 + 512]
-    call print_hex
-    call print_crlf
-    mov dl, [0x9000 + 512 + 511]
-    call print_hex
-    call print_crlf
-    mov dl, [0x9000 + 512 + 512]
-    call print_hex
-    call print_crlf
-
-
-
-
-; Infinite loop (e9 fd ff)
+    call switch_to_pm
+    jmp $  ; we will never reach this, the code will jump to protected_mode_started
+    
+[bits 32]
+protected_mode_started:
+    ; we will be jumped to here after we have switched to protected mode.
+    mov ebx, protected_mode_message
+    call print_string_protected_mode
     jmp $
 
 
+
+
 ; messages are zero terminated, to easily detect their end
-hello_message:
-    db 'Boot sector running', 0
+real_mode_message:
+    db 'Real mode code running', 0
+protected_mode_message:
+    db 'Protected mode code running', 0
 
 
 
+
+
+
+[bits 16]
 ; function to print a string.
 ; string to be printed must be pointed by bx
 ; essentially we do "while (string[i] != 0) { print string[i]; i++ }"
@@ -206,6 +201,98 @@ mismatch_msg: db 'Incorrect sectors read', 0
 
 
 
+[bits 32]
+VIDEO_MEMORY equ 0xb8000
+WHITE_ON_BLACK_COLOR equ 0x0f
+
+; function to print something in protected mode.
+; we can no longer use int 0x10, so we write to video memory directly
+; EBX points to the string we want to print
+print_string_protected_mode:
+    pusha
+    mov edx, VIDEO_MEMORY
+
+print_string_pm_next:
+    mov al, [ebx]
+    mov ah, WHITE_ON_BLACK_COLOR
+    cmp al, 0   ; see if we finished
+    je print_string_pm_done
+
+    mov [edx], ax  ; copy word to video memory
+    add ebx, 1     ; next character in string
+    add edx, 2     ; skip character and color in video memory
+    jmp print_string_pm_next
+
+print_string_pm_done:
+    popa
+    ret
+
+
+[bits 16]
+; setting up the GDT, to enable the CPU to switch to protected mode
+; actually mostly copied from the net, i did not write them myself so much
+gdt_start:
+    ; the GDT starts with a null 8-byte
+    dd 0x0 ; 4 byte
+    dd 0x0 ; 4 byte
+
+; GDT for code segment. base = 0x00000000, length = 0xfffff
+gdt_code: 
+    dw 0xffff    ; segment length, bits 0-15
+    dw 0x0       ; segment base, bits 0-15
+    db 0x0       ; segment base, bits 16-23
+    db 10011010b ; flags (8 bits)
+    db 11001111b ; flags (4 bits) + segment length, bits 16-19
+    db 0x0       ; segment base, bits 24-31
+
+; GDT for data segment. base and length identical to code segment
+; some flags changed, again, refer to os-dev.pdf
+gdt_data:
+    dw 0xffff
+    dw 0x0
+    db 0x0
+    db 10010010b
+    db 11001111b
+    db 0x0
+
+gdt_end:
+
+; GDT descriptor
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1 ; size (16 bit), always one less of its true size
+    dd gdt_start ; address (32 bit)
+
+; define some constants for later use
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
+
+
+
+
+
+; real mode code to switch to protected mode
+[bits 16]
+switch_to_pm:
+    cli                     ; 1. disable interrupts
+    lgdt [gdt_descriptor]   ; 2. load the GDT descriptor
+    mov eax, cr0
+    or eax, 0x1             ; 3. set 32-bit mode bit in cr0
+    mov cr0, eax
+    jmp CODE_SEG:init_pm    ; 4. far jump by using a different segment (must be far jump)
+[bits 32]
+init_pm:                    ; we are now using 32-bit instructions
+    mov ax, DATA_SEG        ; 5. update the segment registers
+    mov ds, ax
+    mov ss, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ebp, 0x90000        ; 6. update the stack right at the top of the free space
+    mov esp, ebp
+    call protected_mode_started   ; 7. Call a well-known label with useful code
+
+
+
 ; Fill with 510 zeros minus the size of the previous code
 times 510-($-$$) db 0
 
@@ -214,16 +301,8 @@ times 510-($-$$) db 0
 dw 0xaa55 
 
 
-
 ; BIOS only loads the first sector, the first 512 bytes.
 ; So, for any code below these lines, we need to load it explicitely
-
-
-
-; fill another sector with a magic number
-times 512 db 0x22
-
-; and another one
-times 512 db 0x44
+; ---------------------------------------------------------------------------
 
 
