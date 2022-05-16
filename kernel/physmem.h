0c74c5fcd09469a2080ec32b0da90f908d5195d1
Dimitris Vidos <dvidos@gmail.com>
2022-05-16 18:35:24 -0400

    Minor header fix

--- kernel/physmem.h
+++ kernel/physmem.h
@@ -8,9 +8,13 @@
 
 
 void init_physical_memory_manager(multiboot_info_t *info, uint8_t *kernel_start_address, uint8_t *kernel_end_address);
+
 void *allocate_physical_page();
-void *allocate_consecutive_physical_pages(size_t size_in_bytes);
 void free_physical_page(void *address);
+
+void *allocate_consecutive_physical_pages(size_t size_in_bytes);
+void free_consecutive_physical_pages(void *address, size_t size_in_bytes);
+
 void dump_physical_memory_map_overall();
 void dump_physical_memory_map_detail(uint32_t start_address);
 
