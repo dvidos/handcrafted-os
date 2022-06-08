e55f9416ecf9017d7aa4cadec3bbe0872fd374fc
Dimitris Vidos <dvidos@gmail.com>
2022-06-07 21:06:40 -0400

    strbuff functionality

--- src/kernel/core/include/memory/kheap.h
+++ src/kernel/core/include/memory/kheap.h
@@ -1,6 +1,8 @@
 #ifndef _KHEAP_H
 #define _KHEAP_H
 
+#include <stddef.h>
+
 
 // naive effort, needs to rewrite completely
 void init_kernel_heap();
--- src/kernel/core/tests.c
+++ src/kernel/core/tests.c
@@ -1,10 +1,12 @@
 #include <drivers/screen.h>
 #include <klib/string.h>
+#include <klib/strbuff.h>
+#include <klog.h>
 
 
 #define assert(a)    \
     if (!(a)) {      \
-        printk("Assertion failed: \"%s\", file %s, line %u\n", #a, __FILE__, __LINE__); \
+        klog_error("Assertion failed: \"%s\", file %s, line %u", #a, __FILE__, __LINE__); \
     } else {         \
         printk("."); \
     }
@@ -12,11 +14,13 @@
 
 void test_strings();
 void test_printf();
+void test_strbuff();
 
 
 void run_tests() {
     test_strings();
     test_printf();
+    test_strbuff();
 }
 
 
@@ -251,4 +255,103 @@ void test_printf() {
     p = (void *)0xFFFFFFFF;  // full 4GB address
     sprintfn(buffer, sizeof(buffer), "%p", p); 
     assert(strcmp(buffer, "FFFFFFFF") == 0);
+}
+
+void test_strbuff() {
+
+    // test a fixed allocation buffer
+    strbuff_t *sb = create_sb(32, SB_FIXED);
+    assert(sb_length(sb) == 0);
+
+    // in order to use it with printf() and other methods
+    sb_strcpy(sb, "coding");
+    assert(sb_charptr(sb) != NULL);
+    assert(strlen(sb_charptr(sb)) == 6);
+    assert(strcmp(sb_charptr(sb), "coding") == 0);
+    sb_clear(sb);
+    assert(sb_length(sb) == 0);
+
+    // try deleting some content
+    sb_strcpy(sb, "Hello there");
+    assert(sb_length(sb) == 11);
+    assert(sb_strcmp(sb, "Hello there") == 0);
+    sb_delete(sb, 4, 1);
+    assert(sb_length(sb) == 10);
+    assert(sb_strcmp(sb, "Hell there") == 0);
+    sb_strcpy(sb, "Hello there");
+    sb_delete(sb, 4, 100);
+    assert(sb_strcmp(sb, "Hell") == 0);
+    sb_strcpy(sb, "Hello there");
+    sb_delete(sb, -5, 100);
+    assert(sb_strcmp(sb, "Hello there") == 0);
+    sb_strcpy(sb, "Hello there");
+    sb_delete(sb, 35, 100);
+    assert(sb_strcmp(sb, "Hello there") == 0);
+
+    // insert simple case
+    sb = create_sb(12, SB_FIXED);
+    sb_strcpy(sb, "hello");
+    sb_append(sb, " there");
+    assert(sb_strcmp(sb, "hello there") == 0);
+    // beyond on the right
+    sb_strcpy(sb, "hello");
+    sb_insert(sb, sb_length(sb), " and goodbye");
+    assert(sb_strcmp(sb, "hello and go") == 0);
+    // beyond on the left
+    sb_strcpy(sb, "hello");
+    sb_insert(sb, 2, " and goodbye");
+    assert(sb_strcmp(sb, "he and goodb") == 0);
+    // make sure further append is ignored
+    sb_append(sb, "further");
+    assert(sb_strcmp(sb, "he and goodb") == 0);
+
+    // insert: test out of boundaries
+    sb_strcpy(sb, "hello");
+    sb_insert(sb, -5, "bye bye!");
+    assert(sb_strcmp(sb, "hello") == 0);
+    sb_strcpy(sb, "hello");
+    sb_insert(sb, 8, "bye bye!");
+    assert(sb_strcmp(sb, "hello") == 0);
+    // test insert huge string
+    sb_strcpy(sb, "test");
+    sb_insert(sb, 2, "abcdefgjklmnopqrstuvwxyz");
+    assert(sb_strcmp(sb, "teabcdefgjkl") == 0)
+    // test insert in the middle, overflowing left
+    sb_strcpy(sb, "123456789012");
+    sb_insert(sb, 5, "abc");
+    assert(sb_strcmp(sb, "12345abc6789") == 0)
+
+
+    // check SB_SCROLL behavior
+    sb_free(sb);
+    sb = create_sb(12, SB_SCROLL);
+    sb_append(sb, "Monday,");
+    assert(sb_strcmp(sb, "Monday,") == 0)
+    sb_append(sb, "Tuesday,");
+    assert(sb_strcmp(sb, "day,Tuesday,") == 0)
+    sb_append(sb, "Wednesday,");
+    assert(sb_strcmp(sb, "y,Wednesday,") == 0)
+    sb_append(sb, "Thursday,");
+    assert(sb_strcmp(sb, "ay,Thursday,") == 0)
+    sb_append(sb, "Friday!");
+    assert(sb_strcmp(sb, "sday,Friday!") == 0)
+    sb_append(sb, "Very big buffer trying to test overflow behavior");
+    assert(sb_strcmp(sb, "low behavior") == 0)
+
+
+    // check SB_EXPAND mode
+    sb = create_sb(12, SB_EXPAND);
+    sb_append(sb, "Monday,");
+    assert(sb_strcmp(sb, "Monday,") == 0)
+    sb_append(sb, "Tuesday,");
+    assert(sb_strcmp(sb, "Monday,Tuesday,") == 0)
+    sb_append(sb, "Wednesday,");
+    assert(sb_strcmp(sb, "Monday,Tuesday,Wednesday,") == 0)
+    sb_append(sb, "Thursday,");
+    assert(sb_strcmp(sb, "Monday,Tuesday,Wednesday,Thursday,") == 0)
+    sb_append(sb, "Friday!");
+    assert(sb_strcmp(sb, "Monday,Tuesday,Wednesday,Thursday,Friday!") == 0)
+    sb_insert(sb, 25, "(insert day here),");
+    assert(sb_strcmp(sb, "Monday,Tuesday,Wednesday,(insert day here),Thursday,Friday!") == 0)
+    sb_free(sb);
 }
