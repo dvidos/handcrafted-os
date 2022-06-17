27d4b0858ff84adbd3469cd9f16a2fa73d543cce
Dimitris Vidos <dvidos@gmail.com>
2022-06-17 19:47:41 -0400

    Moving of disk images

--- emu.sh
+++ emu.sh
@@ -17,11 +17,12 @@ qemu-system-i386 \
     -chardev stdio,id=char0,logfile=qemu.log,signal=off \
     -serial chardev:char0 \
     \
-    -drive id=disk1,file=linux_disk.img,if=none \
+    -drive id=disk1,file=imgs/linux_disk.img,if=none \
     -device ahci,id=ahci \
     -device ide-hd,drive=disk1,bus=ahci.0 \
     \
-    -drive file=dos_disk.img,if=ide,index=0,media=disk,format=raw  \
+    -drive file=imgs/dos_disk.img,if=ide,index=0,media=disk,format=raw  \
+    -drive file=imgs/disk10mb-fat16.img,if=ide,index=3,media=disk,format=raw  \
     -drive file=handcrafted-os.iso,if=ide,index=2,media=cdrom    \
     \
     -boot order=d
