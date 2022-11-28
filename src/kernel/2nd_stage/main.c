2ac56b60218bf26a8d9c70377d7805b27e1b896e
Dimitris Vidos <dvidos@gmail.com>
2022-11-27 21:53:34 -0500

    Better 2 stages boot loaders files

--- src/kernel/2nd_stage/main.c
+++ src/kernel/2nd_stage/main.c
@@ -6,11 +6,69 @@
     find, load and execute final kernel
 
     this second stage boot loader will be loaded in 0x10000 (i.e. 64K)
+
+    maybe this will help: https://dc0d32.blogspot.com/2010/06/real-mode-in-c-with-gcc-writing.html
+    or this: http://www.cs.cmu.edu/~410-s07/p4/p4-boot.pdf
+    definitely these
 */
 
 #define NULL   0
+typedef unsigned short u16;
 
 void print_string(char *s);
+
+
+u16 bios_screen_functions(u16 ax) {
+    u16 status;
+    asm(
+        "movl %1, %%ax \n\t"
+        "int $0x10 \n\t"
+        "movw %%ax, %0 \n\t"
+        : "=g" (status)
+        : "g" (ax)
+        : "%ax" 
+    );
+    return status;
+}
+
+u16 bios_keyboard_functions(u16 ax) {
+    u16 status;
+    asm(
+        "movl %1, %%ax \n\t"
+        "int $0x16 \n\t"
+        "movw %%ax, %0 \n\t"
+        : "=g" (status)
+        : "g" (ax)
+        : "%ax" 
+    );
+    return status;
+}
+
+u16 bios_memory_functions(u16 ax) {
+    u16 status;
+    asm(
+        "movl %1, %%ax \n\t"
+        "int $0x15 \n\t"
+        "movw %%ax, %0 \n\t"
+        : "=g" (status)
+        : "g" (ax)
+        : "%ax" 
+    );
+    return status;
+}
+u16 bios_disk_functions(u16 ax) {
+    u16 status;
+    asm(
+        "movl %1, %%ax \n\t"
+        "int $0x13 \n\t"
+        "movw %%ax, %0 \n\t"
+        : "=g" (status)
+        : "g" (ax)
+        : "%ax" 
+    );
+    return status;
+}
+
 
 void do_something(int arg)
 {
---
+++ src/kernel/2nd_stage/readme.md
@@ -0,0 +1,33 @@
+
+# second stage boot loader
+
+Essentially to be loaded by the small boot sector code
+
+Can be any number of sectors long (e.g. can easily be 16 KB).
+Planning to load it on 0x1000 (64KB).
+
+Needs to be a mix of assembly and C (though it's difficult to emit real mode C). 
+Also, our linked package should be fairly simple, e.g. no ELF headers.
+Data segment could be after code if we wanted to.
+The `start` function must be the first code in the first section of the image, 
+so that our first stage boot loader can jump to it.
+
+Needs to be stored at a known point in the disk (sector based, not file system based)
+
+See the following articles:
+
+* https://wiki.osdev.org/Rolling_Your_Own_Bootloader
+* https://wiki.osdev.org/BIOS
+* https://wiki.osdev.org/My_Bootloader_Does_Not_Work
+* https://blog.ghaiklor.com/2017/11/02/how-to-implement-a-second-stage-boot-loader/
+* https://dc0d32.blogspot.com/2010/06/real-mode-in-c-with-gcc-writing.html
+* http://www.cs.cmu.edu/~410-s07/p4/p4-boot.pdf
+
+It has to do the following things, roughly:
+
+* Get information from BIOS (e.g. memory sizes)
+* Probe and maybe enter graphics mode
+* Parse filesystem (FAT? ext2?) to find the `/boot/kernel` file
+* Parse elf header and load the kernel on 0x100000 (1MB)
+* Prepare a multiboot2 environment and run the kernel.
+
