9e976f6e196f26e00d96a4b29aef602aed25572c
Dimitris Vidos <dvidos@gmail.com>
2025-11-15 09:48:50 -0500

    Compiing correctly

--- src/kernel/filesys/simplest/sector_device.h
+++ src/kernel/filesys/simplest/sector_device.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stdint.h>
+
 typedef struct sector_device      sector_device;
 
 
@@ -7,8 +9,8 @@ typedef struct sector_device {
     uint32_t (*get_sector_size)(sector_device *device);  // typically 512 bytes
     uint32_t (*get_sector_count)(sector_device *device); // how many sectors
 
-    int (*read_sector)(sector_device *device, uint32_t sector_no, char *buffer);
-    int (*write_sector)(sector_device *device, uint32_t sector_no, char *buffer);
+    int (*read_sector)(sector_device *device, uint32_t sector_no, uint8_t *buffer);
+    int (*write_sector)(sector_device *device, uint32_t sector_no, uint8_t *buffer);
 
     void *device_data;
 } sector_device;
--- src/kernel/filesys/simplest/simplest_filesystem.c
+++ src/kernel/filesys/simplest/simplest_filesystem.c
@@ -2,6 +2,7 @@
 #include "simplest_filesystem.h"
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
 
 #define ceiling_division(x, y)    (((x) + ((y)-1)) / (y))
 
@@ -40,13 +41,13 @@
 
 // -------------------------------------------------------
 
-typedef struct simplest_filesystem_data simplest_filesystem_data;
+typedef struct filesys_data filesys_data;
 typedef struct runtime_data runtime_data;
 typedef struct superblock superblock;
 typedef struct block_range block_range;
 typedef struct inode inode;
 
-struct simplest_filesystem_data {
+struct filesys_data {
     sector_device *device;
     runtime_data *runtime; // if non-null, the filesystem is mounted
 };
@@ -58,19 +59,9 @@ struct runtime_data {
     uint8_t *scratch_block_buffer;
 };
 
-struct superblock { // must be up to 512 bytes, in order to read from unknown device
-    char magic[4];  // e.g. "SFS1" version can be in here
-    uint32_t sectors_per_block;    // typically 1-8, for a block size of 512..4kB
-    uint32_t block_size_in_bytes;  // typically 512, 1024, 2048 or 4096.
-    uint32_t blocks_in_device;     // typically 2k..10M
-    uint32_t used_blocks_bitmap_first_block;  // typically block 1 (0 is superblock)
-    uint32_t used_blocks_bitmap_blocks_count;       // typically 1 through 16 blocks
-
-    uint8_t padding[512 - 4 - (sizeof(uint32_t) * 5) - (sizeof(inode) * 2)];
-
-    // these two 2*64=128 bytes, still 1/4 of a block...
-    inode inodes_database; // file with inodes. inode_no is the record number, zero based.
-    inode root_directory;  // file with the entries for root directory. 
+struct block_range { // target size: 8
+    uint32_t first_block_no;
+    uint32_t blocks_count;
 };
 
 struct inode { // target size: 64
@@ -84,32 +75,25 @@ struct inode { // target size: 64
     uint32_t indirect_ranges_block_no;
 };
 
-struct block_range { // target size: 8
-    uint32_t first_block_no;
-    uint32_t blocks_count;
+struct superblock { // must be up to 512 bytes, in order to read from unknown device
+    char magic[4];  // e.g. "SFS1" version can be in here
+    uint32_t sector_size;          // typically 512 to 4K, device driven
+    uint32_t sectors_per_block;    // typically 1-8, for a block size of 512..4kB
+    uint32_t block_size_in_bytes;  // typically 512, 1024, 2048 or 4096.
+    uint32_t blocks_in_device;     // typically 2k..10M
+    uint32_t used_blocks_bitmap_first_block;  // typically block 1 (0 is superblock)
+    uint32_t used_blocks_bitmap_blocks_count;       // typically 1 through 16 blocks
+
+    uint8_t padding[512 - 4 - (sizeof(uint32_t) * 6) - (sizeof(inode) * 2)];
+
+    // these two 2*64=128 bytes, still 1/4 of a block...
+    inode inodes_database; // file with inodes. inode_no is the record number, zero based.
+    inode root_directory;  // file with the entries for root directory. 
 };
 
 // ------------------------------------------------------------------
 
-simplest_filesystem *new_simplest_filesystem(sector_device *device) {
-
-    simplest_filesystem_data *sfs_data = malloc(sizeof(simplest_filesystem_data));
-    sfs_data->device = device;
-    sfs_data->runtime = NULL; // we are not mounted, for now
-
-    simplest_filesystem *sfs = malloc(sizeof(simplest_filesystem));
-    sfs->mkfs = sfs_mkfs;
-    sfs->mount = sfs_mount;
-    sfs->sync = sfs_sync;
-    sfs->unmount = sfs_unmount;
-    sfs->sfs_data = sfs_data;
-
-    return sfs;
-}
-
-// ------------------------------------------------------------------
-
-static int initialize_superblock(uint32_t sector_size, uint32_t sector_count, superblock *superblock) {
+static int populate_superblock(uint32_t sector_size, uint32_t sector_count, superblock *superblock) {
     superblock->magic[0] = 'S';
     superblock->magic[1] = 'F';
     superblock->magic[2] = 'S';
@@ -141,6 +125,7 @@ static int initialize_superblock(uint32_t sector_size, uint32_t sector_count, su
     if (blk_size % sector_size != 0)
         return ERR_NOT_SUPPORTED;
     
+    superblock->sector_size = sector_size;
     superblock->block_size_in_bytes = blk_size;
     superblock->sectors_per_block = blk_size / sector_size;
     superblock->blocks_in_device = (uint32_t)(capacity / blk_size);
@@ -153,27 +138,47 @@ static int initialize_superblock(uint32_t sector_size, uint32_t sector_count, su
     return OK;
 }
 
-static int read_block_range(sector_device *dev, superblock *sb, uint32_t first_block, uint32_t block_count, void *buffer) {
+static int read_block_range_low_level(sector_device *dev, uint32_t sector_size, uint32_t sectors_per_block, uint32_t first_block, uint32_t block_count, void *buffer) {
     // very simple to allow us loading the superblock on mounting
-    if (sb == NULL)
+    if (dev == NULL)
         return ERR_NOT_SUPPORTED;
-    if (first_block >= sb->blocks_in_device || first_block + block_count - 1 >= sb->blocks_in_device)
-        return ERR_OUT_OF_BOUNDS;
 
-    int first_sector = first_block * sb->sectors_per_block;
-    int sector_count = block_count * sb->sectors_per_block;
-    for (int i = 0; i < sector_count; i++) {
-        int err = dev->read_sector(dev, first_sector + i, buffer);
+    int sector_number = first_block * sectors_per_block;
+    int sector_count = block_count * sectors_per_block;
+    void *target = buffer;
+    while (sector_count-- > 0) {
+        int err = dev->read_sector(dev, sector_number, target);
         if (err != OK) return err;
+        sector_number += 1;
+        target += sector_size;
     }
 
     return OK;
 }
 
-static int write_block_range(simplest_filesystem_data *data, uint32_t first_block, uint32_t block_count, void *buffer) {
+static int read_block_range(filesys_data *data, uint32_t first_block, uint32_t block_count, void *buffer) {
+    if (data->runtime == NULL || data->runtime->superblock == NULL)
+        return ERR_NOT_SUPPORTED;
+    superblock *sb = data->runtime->superblock;
+    if (sb == NULL)
+        return ERR_NOT_SUPPORTED;
+    if (first_block >= sb->blocks_in_device || first_block + block_count - 1 >= sb->blocks_in_device)
+        return ERR_OUT_OF_BOUNDS;
+    
+    return read_block_range_low_level(
+        data->device,
+        sb->sector_size,
+        sb->sectors_per_block,
+        first_block,
+        block_count,
+        buffer
+    );
+}
+
+static int write_block_range(filesys_data *data, uint32_t first_block, uint32_t block_count, void *buffer) {
     if (data->runtime == NULL)
         return ERR_NOT_SUPPORTED;
-    if (data->runtime->readonly == NULL)
+    if (data->runtime->readonly)
         return ERR_NOT_PERMITTED;
     superblock *sb = data->runtime->superblock;
     if (sb == NULL)
@@ -181,20 +186,23 @@ static int write_block_range(simplest_filesystem_data *data, uint32_t first_bloc
     if (first_block >= sb->blocks_in_device || first_block + block_count - 1 >= sb->blocks_in_device)
         return ERR_OUT_OF_BOUNDS;
 
+    int sector_size = sb->sector_size;
     int first_sector = first_block * sb->sectors_per_block;
     int sector_count = block_count * sb->sectors_per_block;
+    void *target = buffer;
     for (int i = 0; i < sector_count; i++) {
-        int err = data->device->write_sector(data->device, first_sector + i, buffer);
+        int err = data->device->write_sector(data->device, first_sector + i, target);
         if (err != OK) return err;
+        target += sector_size;
     }
 
     return OK;
 }
 
-static int copy_block_range(simplest_filesystem_data *data, uint32_t source_first_block, uint32_t dest_first_block, uint32_t block_count) {
+static int copy_block_range(filesys_data *data, uint32_t source_first_block, uint32_t dest_first_block, uint32_t block_count) {
     if (data->runtime == NULL || data->runtime->superblock == NULL)
         return ERR_NOT_SUPPORTED;
-    if (data->runtime->readonly == NULL)
+    if (data->runtime->readonly)
         return ERR_NOT_PERMITTED;
     superblock *sb = data->runtime->superblock;
     if (sb == NULL)
@@ -206,20 +214,23 @@ static int copy_block_range(simplest_filesystem_data *data, uint32_t source_firs
     if (source_first_block == dest_first_block || block_count == 0)
         return OK;
 
-    int source_first_sector = source_first_block * sb->sectors_per_block;
-    int dest_first_sector = dest_first_block * sb->sectors_per_block;
+    int err;
+    int source_sector = source_first_block * sb->sectors_per_block;
+    int dest_sector = dest_first_block * sb->sectors_per_block;
     int sector_count = block_count * sb->sectors_per_block;
-    for (int i = 0; i < sector_count; i++) {
-        int err = data->device->read_sector(data->device, source_first_sector + i, data->runtime->scratch_block_buffer);
+    while (sector_count-- > 0) {
+        err = data->device->read_sector(data->device, source_sector, data->runtime->scratch_block_buffer);
         if (err != OK) return err;
-        int err = data->device->write_sector(data->device, dest_first_block + i, data->runtime->scratch_block_buffer);
+        err = data->device->write_sector(data->device, dest_sector, data->runtime->scratch_block_buffer);
         if (err != OK) return err;
+        source_sector += 1;
+        dest_sector += 1;
     }
 
     return OK;
 }
 
-static int is_block_used(simplest_filesystem_data *data, uint32_t block_no) {
+static int is_block_used(filesys_data *data, uint32_t block_no) {
     if (data->runtime == NULL)
         return ERR_NOT_SUPPORTED;
     superblock *sb = data->runtime->superblock;
@@ -233,7 +244,7 @@ static int is_block_used(simplest_filesystem_data *data, uint32_t block_no) {
     return data->runtime->used_blocks_bitmap[byte_no] & (0x01 << bit_no);
 }
 
-static int mark_block_used(simplest_filesystem_data *data, uint32_t block_no) {
+static int mark_block_used(filesys_data *data, uint32_t block_no) {
     if (data->runtime == NULL)
         return ERR_NOT_SUPPORTED;
     if (data->runtime->readonly)
@@ -250,7 +261,7 @@ static int mark_block_used(simplest_filesystem_data *data, uint32_t block_no) {
     return OK;
 }
 
-static int mark_block_free(simplest_filesystem_data *data, uint32_t block_no) {
+static int mark_block_free(filesys_data *data, uint32_t block_no) {
     if (data->runtime == NULL)
         return ERR_NOT_SUPPORTED;
     if (data->runtime->readonly)
@@ -279,16 +290,16 @@ static int sfs_fsck(simplest_filesystem *sfs, int verbose, int repair) {
 }
 
 static int sfs_mount(simplest_filesystem *sfs, int readonly) {
-    simplest_filesystem_data *data = (simplest_filesystem_data *)sfs->sfs_data;
+    filesys_data *data = (filesys_data *)sfs->sfs_data;
     if (data->runtime != NULL)
         return ERR_NOT_SUPPORTED;
 
     // first check the first sector to see if we can mount it.
     // hence why the superblock must be at most 512 bytes size.
-    char *temp_sector = malloc(data->device->get_sector_size(data->device));
+    uint8_t *temp_sector = malloc(data->device->get_sector_size(data->device));
     data->device->read_sector(data->device, 0, temp_sector);
     superblock *tmpsb = (superblock *)temp_sector;
-    int recognized = memcm(tmpsb->magic, "SFS1", 4) == 0;
+    int recognized = memcmp(tmpsb->magic, "SFS1", 4) == 0;
     if (!recognized) {
         free(temp_sector);
         return ERR_NOT_RECOGNIZED;
@@ -302,17 +313,25 @@ static int sfs_mount(simplest_filesystem *sfs, int readonly) {
     runtime->scratch_block_buffer = malloc(tmpsb->block_size_in_bytes);
     data->runtime = runtime;
     
-    // read superblock
-    int err = read_block_range(data->device, tmpsb, 0, 1, data->runtime->superblock);
+    // read superblock, low level, since we are unmounted
+    int err = read_block_range_low_level(
+        data->device,
+        tmpsb->sector_size,
+        tmpsb->sectors_per_block,
+        0, // first block
+        1, // block count
+        data->runtime->superblock
+    );
 
-    // make sure to free, now that we're done
-    free(temp_sector); 
+    // make sure to free early, before anything else
+    free(temp_sector);
     if (err != OK) return err;
 
     // read used blocks bitmap from disk
-    err = read_block_range(
+    err = read_block_range_low_level(
         data->device, 
-        data->runtime->superblock, 
+        data->runtime->superblock->sector_size,
+        data->runtime->superblock->sectors_per_block,
         data->runtime->superblock->used_blocks_bitmap_first_block, 
         data->runtime->superblock->used_blocks_bitmap_blocks_count, 
         data->runtime->used_blocks_bitmap
@@ -323,18 +342,20 @@ static int sfs_mount(simplest_filesystem *sfs, int readonly) {
 }
 
 static int sfs_sync(simplest_filesystem *sfs) {
-    simplest_filesystem_data *data = (simplest_filesystem_data *)sfs->sfs_data;
+    int err;
+
+    filesys_data *data = (filesys_data *)sfs->sfs_data;
     if (data->runtime == NULL)
         return ERR_NOT_SUPPORTED;
     if (data->runtime->readonly)
         return ERR_NOT_PERMITTED;
 
     // write superblock
-    int err = write_block_range(sfs, 0, 1, data->runtime->superblock);
+    err = write_block_range(sfs->sfs_data, 0, 1, data->runtime->superblock);
     if (err != OK) return err;
 
     // write used blocks bitmap to disk
-    int err = write_block_range(sfs, 
+    err = write_block_range(sfs->sfs_data, 
         data->runtime->superblock->used_blocks_bitmap_first_block, 
         data->runtime->superblock->used_blocks_bitmap_blocks_count, 
         data->runtime->used_blocks_bitmap
@@ -345,12 +366,12 @@ static int sfs_sync(simplest_filesystem *sfs) {
 }
 
 static int sfs_unmount(simplest_filesystem *sfs) {
-    simplest_filesystem_data *data = (simplest_filesystem_data *)sfs->sfs_data;
+    filesys_data *data = (filesys_data *)sfs->sfs_data;
     if (data->runtime == NULL)
         return ERR_NOT_SUPPORTED;
 
     if (!data->runtime->readonly) {
-        int err = sync(sfs);
+        int err = sfs_sync(sfs);
         if (err != OK) return err;
     }
 
@@ -365,34 +386,27 @@ static int sfs_unmount(simplest_filesystem *sfs) {
 
 // ------------------------------------------------------------------
 
+simplest_filesystem *new_simplest_filesystem(sector_device *device) {
 
+    if (sizeof(inode) != 64) // written on disk, must be same size
+        return NULL;
+    if (sizeof(superblock) != 512) // must be able to load in a single 512B sector
+        return NULL;
 
+    filesys_data *sfs_data = malloc(sizeof(filesys_data));
+    sfs_data->device = device;
+    sfs_data->runtime = NULL; // we are not mounted, for now
 
+    simplest_filesystem *sfs = malloc(sizeof(simplest_filesystem));
+    sfs->mkfs = sfs_mkfs;
+    sfs->mount = sfs_mount;
+    sfs->sync = sfs_sync;
+    sfs->unmount = sfs_unmount;
+    sfs->fsck = sfs_fsck;
+    sfs->sfs_data = sfs_data;
 
+    return sfs;
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// ------------------------------------------------------------------
 
