0b27d0ee5255b1a04baf8dbd52dfae42597c5c91
Dimitris Vidos <dvidos@gmail.com>
2026-01-07 21:05:26 -0500

    Now understanding keys

--- nursery/graphics/try2/src/kernel/architecture/hosted_x86_64/hosted.c
+++ nursery/graphics/try2/src/kernel/architecture/hosted_x86_64/hosted.c
@@ -44,6 +44,103 @@ void sdl_shutdown(void) {
     SDL_Quit();
 }
 
+keycode_t sdl_scancode_to_kernel_keycode(SDL_Scancode scancode) {
+    switch (scancode) {
+        case SDL_SCANCODE_A: return KEY_A;
+        case SDL_SCANCODE_B: return KEY_B;
+        case SDL_SCANCODE_C: return KEY_C;
+        case SDL_SCANCODE_D: return KEY_D;
+        case SDL_SCANCODE_E: return KEY_E;
+        case SDL_SCANCODE_F: return KEY_F;
+        case SDL_SCANCODE_G: return KEY_G;
+        case SDL_SCANCODE_H: return KEY_H;
+        case SDL_SCANCODE_I: return KEY_I;
+        case SDL_SCANCODE_J: return KEY_J;
+        case SDL_SCANCODE_K: return KEY_K;
+        case SDL_SCANCODE_L: return KEY_L;
+        case SDL_SCANCODE_M: return KEY_M;
+        case SDL_SCANCODE_N: return KEY_N;
+        case SDL_SCANCODE_O: return KEY_O;
+        case SDL_SCANCODE_P: return KEY_P;
+        case SDL_SCANCODE_Q: return KEY_Q;
+        case SDL_SCANCODE_R: return KEY_R;
+        case SDL_SCANCODE_S: return KEY_S;
+        case SDL_SCANCODE_T: return KEY_T;
+        case SDL_SCANCODE_U: return KEY_U;
+        case SDL_SCANCODE_V: return KEY_V;
+        case SDL_SCANCODE_W: return KEY_W;
+        case SDL_SCANCODE_X: return KEY_X;
+        case SDL_SCANCODE_Y: return KEY_Y;
+        case SDL_SCANCODE_Z: return KEY_Z;
+        case SDL_SCANCODE_1: return KEY_1;
+        case SDL_SCANCODE_2: return KEY_2;
+        case SDL_SCANCODE_3: return KEY_3;
+        case SDL_SCANCODE_4: return KEY_4;
+        case SDL_SCANCODE_5: return KEY_5;
+        case SDL_SCANCODE_6: return KEY_6;
+        case SDL_SCANCODE_7: return KEY_7;
+        case SDL_SCANCODE_8: return KEY_8;
+        case SDL_SCANCODE_9: return KEY_9;
+        case SDL_SCANCODE_0: return KEY_0;
+        case SDL_SCANCODE_RETURN: return KEY_ENTER;
+        case SDL_SCANCODE_ESCAPE: return KEY_ESCAPE;
+        case SDL_SCANCODE_BACKSPACE: return KEY_BACKSPACE;
+        case SDL_SCANCODE_TAB: return KEY_TAB;
+        case SDL_SCANCODE_SPACE: return KEY_SPACE;
+        case SDL_SCANCODE_MINUS: return KEY_MINUS;
+        case SDL_SCANCODE_EQUALS: return KEY_EQUAL;
+        case SDL_SCANCODE_LEFTBRACKET: return KEY_LBRACKET;
+        case SDL_SCANCODE_RIGHTBRACKET: return KEY_RBRACKET;
+        case SDL_SCANCODE_BACKSLASH: return KEY_BACKSLASH;
+        case SDL_SCANCODE_SEMICOLON: return KEY_SEMICOLON;
+        case SDL_SCANCODE_APOSTROPHE: return KEY_APOSTROPHE;
+        case SDL_SCANCODE_GRAVE: return KEY_BACKTICK;
+        case SDL_SCANCODE_COMMA: return KEY_COMMA;
+        case SDL_SCANCODE_PERIOD: return KEY_DOT;
+        case SDL_SCANCODE_SLASH: return KEY_SLASH;
+        case SDL_SCANCODE_CAPSLOCK: return KEY_CAPSLOCK;
+        case SDL_SCANCODE_F1: return KEY_F1;
+        case SDL_SCANCODE_F2: return KEY_F2;
+        case SDL_SCANCODE_F3: return KEY_F3;
+        case SDL_SCANCODE_F4: return KEY_F4;
+        case SDL_SCANCODE_F5: return KEY_F5;
+        case SDL_SCANCODE_F6: return KEY_F6;
+        case SDL_SCANCODE_F7: return KEY_F7;
+        case SDL_SCANCODE_F8: return KEY_F8;
+        case SDL_SCANCODE_F9: return KEY_F9;
+        case SDL_SCANCODE_F10: return KEY_F10;
+        case SDL_SCANCODE_F11: return KEY_F11;
+        case SDL_SCANCODE_F12: return KEY_F12;
+        case SDL_SCANCODE_INSERT: return KEY_INSERT;
+        case SDL_SCANCODE_HOME: return KEY_HOME;
+        case SDL_SCANCODE_PAGEUP: return KEY_PAGEUP;
+        case SDL_SCANCODE_DELETE: return KEY_DELETE;
+        case SDL_SCANCODE_END: return KEY_END;
+        case SDL_SCANCODE_PAGEDOWN: return KEY_PAGEDOWN;
+        case SDL_SCANCODE_RIGHT: return KEY_RIGHT;
+        case SDL_SCANCODE_LEFT: return KEY_LEFT;
+        case SDL_SCANCODE_DOWN: return KEY_DOWN;
+        case SDL_SCANCODE_UP: return KEY_UP;
+        case SDL_SCANCODE_KP_DIVIDE: return KEY_KP_DIV;
+        case SDL_SCANCODE_KP_MULTIPLY: return KEY_KP_MUL;
+        case SDL_SCANCODE_KP_MINUS: return KEY_KP_MINUS;
+        case SDL_SCANCODE_KP_PLUS: return KEY_KP_PLUS;
+        case SDL_SCANCODE_KP_ENTER: return KEY_KP_ENTER;
+        case SDL_SCANCODE_KP_1: return KEY_KP_1;
+        case SDL_SCANCODE_KP_2: return KEY_KP_2;
+        case SDL_SCANCODE_KP_3: return KEY_KP_3;
+        case SDL_SCANCODE_KP_4: return KEY_KP_4;
+        case SDL_SCANCODE_KP_5: return KEY_KP_5;
+        case SDL_SCANCODE_KP_6: return KEY_KP_6;
+        case SDL_SCANCODE_KP_7: return KEY_KP_7;
+        case SDL_SCANCODE_KP_8: return KEY_KP_8;
+        case SDL_SCANCODE_KP_9: return KEY_KP_9;
+        case SDL_SCANCODE_KP_0: return KEY_KP_0;
+        case SDL_SCANCODE_KP_PERIOD: return KEY_KP_DOT;
+    }
+    return KEY_NONE;
+}
+
 int convert_sdl_event(SDL_Event *sdl, event_t *ev, bool *quit) {
     memset(ev, 0, sizeof(event_t));
     
@@ -56,13 +153,12 @@ int convert_sdl_event(SDL_Event *sdl, event_t *ev, bool *quit) {
     case SDL_KEYDOWN:
         keymods_t m = 0;
         
-        keycode_t keyboard_keycode_from(int scancode, int is_e0);
         char keyboard_ascii_from(keycode_t keycode, keymods_t modifiers);
 
         ev->type = EVT_KEY;
         ev->key = (key_event_t){
             .type = (sdl->type == SDL_KEYDOWN) ? KEY_PRESSED : KEY_RELEASED,
-            .keycode = keyboard_keycode_from(sdl->key.keysym.scancode, 0),
+            .keycode = sdl_scancode_to_kernel_keycode(sdl->key.keysym.scancode),
         };
         SDL_Keymod sm = SDL_GetModState();
         if (sm & KMOD_CTRL)  ev->key.keymods |= KEY_CTRL;
