b430225bc5be1d7bea4c4f3feafb551443723afa
Dimitris Vidos <dvidos@gmail.com>
2026-02-02 19:00:43 -0500

    New physical memory manager

---
+++ nursery/build-attempt3/kernel/memory/physmem2.c
@@ -0,0 +1,248 @@
+#include "../utils/panic.h"
+#include "../misc/cpu.h"
+#include "physmem2.h"
+
+// 4GB of memory / 4K page size --> 1M pages
+// 1M pages / 32 bits in an uint32_t  => 32K uint32_t numbers.
+// 4K page size / 4 bytes that a uint32_t takes -> 1K uint32_t per page
+// so, using 32 pages, we can have a "used" bit for each page in memory
+// the initialization process is:
+// - mark everything used.
+// - mark availabile memory (e820) as free
+// - mark things we know (kernel, kheap, the used pages bitmap etc) as used
+// - mark the pages of the bitmap as used
+
+
+#define PAGE_SIZE   4096
+
+struct pmm_data_t {
+    
+    // we need to know the address of the 32 pages that contain the "used" bitmap
+    uint32_t total_pages;
+    uint32_t free_pages;
+    uint32_t *bitmap; // 32K uint32_t long, or 32 pages of 4K each
+    uint32_t bitmap_uint_count;
+    uint32_t next_allocation_page_hint;
+    pmm_allocator_t allocator;
+};
+
+static struct pmm_data_t pmm_data;
+
+static phys_addr_t _allocate_physical_page();
+static void _free_physical_page(phys_addr_t addr);
+
+static inline uint32_t round_up_4k(uint32_t address) {
+    return ((address + PAGE_SIZE - 1) / PAGE_SIZE) * PAGE_SIZE;
+}
+
+static inline uint32_t round_down_4k(uint32_t address) {
+    return (address / PAGE_SIZE) * PAGE_SIZE;
+}
+
+static inline uint32_t page_no_for_address(phys_addr_t address) {
+    return address / PAGE_SIZE;
+}
+
+static inline phys_addr_t address_of_page(uint32_t page_no) {
+    return page_no * PAGE_SIZE;
+}
+
+static inline bool is_page_used(uint32_t page_no) {
+    return pmm_data.bitmap[page_no / 32] & ((uint32_t)1 << (page_no % 32));
+}
+
+static inline bool is_page_free(uint32_t page_no) {
+    return !is_page_used(page_no);
+}
+
+static inline void mark_page_used(uint32_t page_no) {
+    if (is_page_used(page_no))
+        return;
+    pmm_data.bitmap[page_no / 32] |= ((uint32_t)1 << (page_no % 32));
+    pmm_data.free_pages--;
+}
+
+static inline void mark_page_free(uint32_t page_no) {
+    if (is_page_used(page_no)) {
+        pmm_data.bitmap[page_no / 32] &= ~((uint32_t)1 << (page_no % 32));
+        pmm_data.free_pages++;
+    }
+}
+
+static void _initialize(phys_addr_t highest_address, phys_addr_t kernel_end) {
+    pmm_data.allocator.allocate_physical_page = _allocate_physical_page;
+    pmm_data.allocator.free_physical_page = _free_physical_page;
+
+    pmm_data.total_pages = (highest_address + 4095) / PAGE_SIZE;
+    pmm_data.bitmap = (uint32_t *)round_up_4k(kernel_end);
+    pmm_data.bitmap_uint_count = (pmm_data.total_pages + 31) / 32;
+    pmm_data.next_allocation_page_hint = 0;
+
+    // initially mark everything as unusable
+    for (uint32_t i = 0; i < pmm_data.bitmap_uint_count; i++)
+        pmm_data.bitmap[i] = 0xFFFFFFFF; // used, or unusable
+    pmm_data.free_pages = 0;
+}
+
+static void _mark_region_available(phys_addr_t start, size_t length) {
+    start = round_down_4k(start);
+    length = round_up_4k(length);
+    phys_addr_t end = start + length;
+    for (phys_addr_t addr = start; addr < end; addr += PAGE_SIZE)
+        mark_page_free(page_no_for_address(addr));
+}
+
+static void _mark_region_reserved(phys_addr_t start, size_t length) {
+    start = round_down_4k(start);
+    length = round_up_4k(length);
+    phys_addr_t end = start + length;
+    for (phys_addr_t addr = start; addr < end; addr += PAGE_SIZE)
+        mark_page_used(page_no_for_address(addr));
+}
+
+static void _finish_initialization() {
+    // mark the pages of the bitmap as reserved
+    phys_addr_t bitmap_start = (phys_addr_t)pmm_data.bitmap;
+    phys_addr_t bitmap_end = (phys_addr_t)(pmm_data.bitmap + pmm_data.bitmap_uint_count);
+    for (phys_addr_t addr = bitmap_start; addr < bitmap_end; addr += PAGE_SIZE)
+        mark_page_used(page_no_for_address(addr));
+
+    // page zero is invalid, null pointers also.
+    mark_page_used(0);
+}
+
+static phys_addr_t _find_next_free_page() {
+    uint32_t index = pmm_data.next_allocation_page_hint / 32;
+    for (uint32_t times = 0; times < pmm_data.bitmap_uint_count; times++) {
+        if (pmm_data.bitmap[index] == 0xFFFFFFFF) {
+            if (++index >= pmm_data.bitmap_uint_count)
+                index = 0;
+            continue;
+        }
+
+        for (int bit = 0; bit < 32; bit++) {
+            uint32_t page_no = index * 32 + bit;
+            if (page_no >= pmm_data.total_pages)
+                continue; // last bits of the last uint32 may not participate
+
+            if (pmm_data.bitmap[index] & (uint32_t)1 << bit)
+                continue;
+            
+            pmm_data.next_allocation_page_hint = (page_no + 1) % pmm_data.total_pages;
+            return page_no;
+        }
+
+        if (++index >= pmm_data.bitmap_uint_count)
+            index = 0;
+    }
+
+    return INVALID_PAGE;
+}
+
+static phys_addr_t _allocate_physical_page() { 
+    phys_addr_t addr = 0;
+    
+    pushcli();
+    int page_no = _find_next_free_page();
+    if (page_no != INVALID_PAGE) {
+        mark_page_used(page_no);
+        addr = address_of_page(page_no);
+    }
+    popcli();
+
+    return addr;
+}
+
+static void _free_physical_page(phys_addr_t addr) { 
+    if (addr != round_down_4k(addr))
+        panic("Freeing physical address not aligned to 4k");
+    uint32_t page_no = page_no_for_address(addr);
+    if (page_no >= pmm_data.total_pages)
+        panic("Freeing page outside of total pages");
+    if (!is_page_used(page_no))
+        panic("Attempt to free a non-allocated page");
+    if (addr >= (phys_addr_t)pmm_data.bitmap && addr < (phys_addr_t)(pmm_data.bitmap + pmm_data.bitmap_uint_count))
+        panic("Attempt to free page in the used pages bitmap");
+    
+    pushcli();
+    mark_page_free(page_no_for_address(addr));
+    popcli();
+}
+
+static phys_addr_t _allocate_consecutive_pages(size_t total_bytes) {
+    if (total_bytes <= PAGE_SIZE)
+        return _allocate_physical_page();
+
+    pushcli();
+
+    phys_addr_t addr = 0;
+    int pages_needed = round_up_4k(total_bytes) / PAGE_SIZE;
+    uint32_t starting_page = pmm_data.next_allocation_page_hint;
+    uint32_t iterations_left = pmm_data.total_pages;
+
+    while (iterations_left-- > 0) {
+        uint32_t first_page = _find_next_free_page();
+        if (first_page == INVALID_PAGE)
+            break;
+        if (first_page + pages_needed > pmm_data.total_pages)
+            continue; // if block does not fit, continue till we wrap around
+
+        bool all_consecutive_free = true;
+        for (int extra = 1; extra < pages_needed; extra++) {
+            if (!is_page_free(first_page + extra)) {
+                all_consecutive_free = false;
+                break;
+            }
+        }
+
+        if (all_consecutive_free) {
+            for (int extra = 0; extra < pages_needed; extra++)
+                mark_page_used(first_page + extra);
+            addr = address_of_page(first_page);
+            break;
+        }
+    }
+
+    popcli();
+    return addr;
+}
+
+static void _free_consecutive_pages(phys_addr_t address, size_t total_bytes) {
+    if (total_bytes <= PAGE_SIZE)
+        _free_physical_page(address);
+
+    pushcli();
+
+    int total_pages = round_up_4k(total_bytes) / PAGE_SIZE;
+    uint32_t first_page = page_no_for_address(address);
+    for (int extra = 0; extra < total_pages; extra++)
+        mark_page_free(first_page + extra);
+
+    popcli();
+}
+
+static uint32_t _total_pages() {
+    return pmm_data.total_pages;
+}
+
+static uint32_t _free_pages() {
+    return pmm_data.free_pages;
+}
+
+static pmm_allocator_t _get_pmm_allocator() {
+    return pmm_data.allocator;
+}
+
+struct pmm_ops pmm = {
+    .initialize = _initialize,
+    .mark_region_available = _mark_region_available,
+    .mark_region_reserved = _mark_region_reserved,
+    .finish_initialization = _finish_initialization,
+    .allocate_physical_page = _allocate_physical_page,
+    .allocate_consecutive_pages = _allocate_consecutive_pages,
+    .free_consecutive_pages = _free_consecutive_pages,
+    .free_physical_page = _free_physical_page,
+    .total_pages = _total_pages,
+    .free_pages = _free_pages,
+    .get_pmm_allocator = _get_pmm_allocator,
+};
---
+++ nursery/build-attempt3/kernel/memory/physmem2.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <ctypes.h>
+
+// maybe i need a better base abstraction for panic. 
+// dedicated header and source file, not needing the VGA driver.
+// we could have the "panic" function just halt, 
+// and then add a "panic_set_message_displayer()" method.
+// this way, we depend on a linked-in panic() method, but nothing else.
+
+
+// to be used as a dependency to other systems
+typedef struct pmm_allocator_t {
+    phys_addr_t (*allocate_physical_page)();
+    void (*free_physical_page)(phys_addr_t addr);
+} pmm_allocator_t;
+
+
+struct pmm_ops {
+    void (*initialize)(phys_addr_t highest_address, phys_addr_t kernel_end);
+    void (*mark_region_available)(phys_addr_t start, size_t length);
+    void (*mark_region_reserved)(phys_addr_t start, size_t length);
+    void (*finish_initialization)();
+
+    phys_addr_t (*allocate_physical_page)();
+    void (*free_physical_page)(phys_addr_t addr);
+    phys_addr_t (*allocate_consecutive_pages)(size_t total_bytes);
+    void (*free_consecutive_pages)(phys_addr_t address, size_t total_bytes);
+
+    uint32_t (*total_pages)();
+    uint32_t (*free_pages)();
+
+    pmm_allocator_t (*get_pmm_allocator)();
+};
+
+
+#define INVALID_PAGE  0
+
+extern struct pmm_ops pmm;
