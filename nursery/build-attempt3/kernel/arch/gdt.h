c954adf71de1b2e2a06b3a2fa9cf8cce228be5ef
Dimitris Vidos <dvidos@gmail.com>
2026-03-21 10:23:42 -0400

    IDT segments

--- nursery/build-attempt3/kernel/arch/gdt.h
+++ nursery/build-attempt3/kernel/arch/gdt.h
@@ -2,6 +2,12 @@
 #define _GDT_H
 
 
+#define KERNEL_CODE_SEGMENT    0x08  //  8, ring 0  executable, read-only
+#define KERNEL_DATA_SEGMENT    0x10  // 16, ring 0, read-write
+#define USER_CODE_SEGMENT      0x18  // 24, ring 3, executable, read-only
+#define USER_DATA_SEGMENT      0x20  // 32, ring 3, read-write
+
+
 void init_gdt();
 
 
