7ee2802e3c5800d86bf2d80689b62aa19fed57a5
Dimitris Vidos <dvidos@gmail.com>
2026-03-21 10:28:23 -0400

    Trap frame creation

---
+++ nursery/build-attempt3/kernel/arch/trap_frame.h
@@ -0,0 +1,47 @@
+#ifndef _TRAP_FRAME_H
+#define _TRAP_FRAME_H
+
+#include "../include/ctypes.h"
+
+
+// things pushed in the isr_stub we have in assembly
+// this is passed when interrupt_handler_c is called from our assembly stub
+// same for all interrupts, including the syscall one (0x80)
+// when assembly pushes ESP and calls C handler, the handler receives
+// the value of ESP, i.e. a pointer to the current stack position.
+// what things have been pushed is decoded by the struct below.
+
+typedef struct trap_frame {
+
+   // pushed by our isr stub, before calling C func
+   uint32_t gs;
+   uint32_t fs;
+   uint32_t es;
+   uint32_t ds;
+
+   uint32_t edi;
+   uint32_t esi;
+   uint32_t ebp;
+   uint32_t esp_dummy;
+   uint32_t ebx;
+   uint32_t edx;
+   uint32_t ecx;
+   uint32_t eax; // Pushed by pusha.
+   
+   // pushed by our assembly macros
+   uint32_t int_no;
+   uint32_t err_code;  // Interrupt number and error code (if applicable)
+   
+   // pushed by CPU before jumping to interrupt entry
+   uint32_t eip;
+   uint32_t cs;
+   uint32_t eflags;
+   uint32_t user_esp;
+   uint32_t ss;
+
+} __attribute__((packed)) trap_frame_t;
+
+
+
+
+#endif
