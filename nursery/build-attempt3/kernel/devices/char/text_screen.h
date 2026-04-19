0a5178a925031b090efda3eaeab1a075e5b60682
Dimitris Vidos <dvidos@gmail.com>
2026-04-19 19:34:48 -0400

    First attempt at text screen

--- nursery/build-attempt3/kernel/devices/char/text_screen.h
+++ nursery/build-attempt3/kernel/devices/char/text_screen.h
@@ -1,7 +1,9 @@
 #include "../include/uapi/base.h"
 
-typedef struct text_screen text_screen_t;
+enum text_color { BLACK, BLUE, GREEN, CYAN, RED, MAGENTA, YELLOW, WHITE };
 
+
+typedef struct text_screen text_screen_t;
 
 struct text_screen {
     struct text_screen_ops *ops;
@@ -23,10 +25,17 @@ struct text_screen_priv_data {
             int begin;
             int end; // exclusive
         } scroll_lines;
+        bool cursor_visible;
 
     } state;
 
+    uint16_t *history_buffer;
+    int history_max_rows;
+    int history_count;
+    int history_head;
+
     uint16_t *buffers[2];
+    int view_offset;
     bool dirty;
 };
 
@@ -43,16 +52,25 @@ struct text_screen_ops {
     void get_size(text_screen_t *s, int *rows, int *cols);
     void set_size(text_screen_t *s, int *rows, int *cols);
 
-    void set_color(text_screen_t *s, uint8_t color);
-    void get_color(text_screen_t *s, uint8_t *color);
+    void set_text_attr(text_screen_t *s, uint8_t color);
+    void get_text_attr(text_screen_t *s, uint8_t *color);
 
     void set_scroll_lines(text_screen_t *s, int begin, int end);
     void get_scroll_lines(text_screen_t *s, int *begin, int *end);
 
-    void set_alt_buffer(bool alt);
-    void get_alt_buffer(bool *alt);
+    void set_alt_buffer(text_screen_t *s, bool alt);
+    void get_alt_buffer(text_screen_t *s, bool *alt);
+
+    void set_cursor_visible(text_screen_t *s, bool visible);
+    void get_cursor_visible(text_screen_t *s, bool *visible);
+
+    void update_hardware_cursor(text_screen_t *s); // outport() to VGA registers
 
     void destroy(text_screen_t *s);
 };
 
 text_screen_t *create_text_screen(int row, int cols);
+
+uint8_t text_fg_color(uint8_t color, enum text_color fg, bool bright);
+uint8_t text_bg_color(uint8_t color, enum text_color bg);
+uint8_t text_attr_of(enum text_color fg, enum text_color bg, bool bright, bool blink);
